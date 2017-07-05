e09fa7c97514240f3a01ac1ab72c1919c8126b37
Adam Morris <amorris@mistermorris.com>
2017-07-05 18:31:28 +0100

    setup file so we can install with pip

---
+++ setup.py
@@ -0,0 +1,18 @@
+from distutils.core import setup
+setup(
+    name = "SPGL",
+    version = "0.7",
+    description = "",
+    keywords = [],
+    install_requires = [],
+
+    classifiers = [
+        "Programming Language :: Python :: 3",
+        "Development Status :: 4 - Beta",
+        ],
+    long_description = """\
+TODO: DESCRIBE THIS!
+
+
+"""
+)
